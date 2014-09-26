8575a81a7e8c2714d81c0c764903c003b75fb9a7
Arthur SFEZ <arthur.sfez@gmail.com>
2014-09-26 19:26:53 +0200

    remove install script

--- install_vim.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-git clone htpps://github.com/gmarik/vundle ~/.vim/bundle/Vundle.vim<br />
-vim +PluginInstall +qall
-ln -s ~/.dotfiles/vimrc ~/.vimrc
-ln -s ~/.dotfiles/gvimrc ~/.gvimrc
-ln -s ~/.dotfiles/vim ~/.vim
