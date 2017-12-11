6bd25dd13ebdbc2b63368973430f3f9f1b7240ec
Arthur SFEZ <arthur.sfez@gmail.com>
2017-12-11 12:05:30 -0800

    submodule fixing vundle

--- install.sh
+++ install.sh
@@ -13,9 +13,9 @@ for file in dot.*; do
 done
 
 echo "Installing vim bundles"
-# vim +BundleInstall +qall
+vim +BundleInstall +qall
 
-read -p "Do you want to install xorg related configurations? " -n 1 -ra
+read -p "Do you want to install xorg related configurations?" -n 1 -ra
 echo
 if [[ $REPLY =~ ^[Yy]$ ]]
 then
