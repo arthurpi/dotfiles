1e8a0dac9b09548cb42d46eee143dc21f571bff3
Arthur Sfez <asfez@google.com>
2022-05-17 00:23:19 -0700

    fish prompt update

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -1,4 +1,10 @@
 if status is-interactive
+
+    # note: this erases the default fish_mode_prompt, which adds a vi mode
+    # indicator to the left of your prompt
+    function fish_mode_prompt; end
+
     # Commands to run in interactive sessions can go here
     set fish_key_bindings fish_user_key_bindings
+
 end
--- config/fish/functions/fish_prompt.fish
+++ config/fish/functions/fish_prompt.fish
@@ -1,31 +1,29 @@
 function fish_prompt
-    set -l last_status $status
-    set -l cwd (prompt_pwd)
+  set -l tomita_vi_mode "$TOMITA_VI"
 
-    if not test $last_status -eq 0
-        set_color --bold white -b red
-        echo -n ' ! '
-        set_color normal
+  if test -z (string match -ri '^no|false|0$' $tomita_vi_mode)
+    switch $fish_bind_mode
+      case default
+        set_color --bold red
+        printf ':'
+      case insert
+        set_color --bold blue
+        printf '+'
+      case visual
+        set_color --bold magenta
+        printf 'v'
     end
-
-    # Display current path
-    set_color black -b blue
-    echo -n " $cwd "
-
-    # Show git branch and dirty state
-    set -l git_branch (command git symbolic-ref HEAD 2> /dev/null | sed -e 's|^refs/heads/||')
-    set -l git_dirty (command git status -s --ignore-submodules=dirty 2> /dev/null)
-    if test -n "$git_branch"
-        if test -n "$git_dirty"
-            set_color black -b yellow
-            echo -n " $git_branch "
-        else
-            set_color black -b green
-            echo -n " $git_branch "
-        end
-    end
-
-    # Add a space
     set_color normal
-    echo -n ' '
+  end
+
+
+	set_color $fish_color_cwd
+  printf '%s' (basename (prompt_pwd))
+
+  set_color normal
+  printf '%s ' (__fish_git_prompt)
+
+  set_color -o yellow
+  printf '$ '
+  set_color normal
 end
