e4e0058445445abfb90996ffb0199a39ebc197b7
Arthur Sfez <asfez@google.com>
2022-05-23 17:58:36 -0700

    show full filename

--- config/nvim/lua/configs/lualine.lua
+++ config/nvim/lua/configs/lualine.lua
@@ -1,6 +1,22 @@
 local lualine = require 'lualine'
 lualine.setup{
     options = { theme = 'seoul256' },
+    sections = {
+        lualine_b = {
+            'branch',
+            {
+                'diff',
+                colored = false,
+            },
+            'diagnostics',
+        },
+        lualine_c = {
+            {
+                'filename',
+                path = 1,
+            },
+        },
+    },
     tabline = {
         lualine_a = {
             {
