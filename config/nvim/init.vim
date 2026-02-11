589af145d2c47e85d44a2b12f8c2f854e011c743
Arthur Sfez <asfez@google.com>
2026-02-11 13:31:00 +0100

    Migration to nvim config: rm vim native plugins

--- config/nvim/init.vim
+++ config/nvim/init.vim
@@ -1,6 +1,5 @@
 set runtimepath^=~/.vim runtimepath+=~/.vim/after
 
-" vim.g.mapleader = ","
 let mapleader = ","
 
 set signcolumn=auto:4                   " enough to show git st and diagnostics
--- install.sh
+++ install.sh
@@ -3,19 +3,8 @@
 # copied from https://github.com/repzret/dotfiles/blob/master/install.sh
 # thanks!
 
-echo "Cloning repos"
-git submodule update --init --recursive
-
 for file in dot.*; do
     name=${file/dot/}
     echo "Installing $name";
     ln -snFi `pwd`/$file ~/$name
 done
-
-read -p "Do you want to install vim bundles now? It will take time! " -n 1 -r
-echo
-if [[ $REPLY =~ ^[Yy]$ ]]
-then
-    echo "Installing vim bundles"
-    vim +BundleInstall +qall
-fi
